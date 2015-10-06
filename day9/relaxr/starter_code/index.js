ab8ca0240dd400420d47a389d7b79f80884aa7c1
baikh321 <habaik@usbosrad8460.global.publicisgroupe.net>
2015-10-06 17:23:11 -0400

    relaxr

--- day9/relaxr/starter_code/index.js
+++ day9/relaxr/starter_code/index.js
@@ -1,23 +1,32 @@
-// $('a.readmore').on('click', function (e) {
+// $('p.readmore a').on('click', function (e) {
 //     e.preventDefault();
 
-// var activeLink = $(this);
-// if (activeLink.hasClass(“.hide”) {
-//     // link is already expanded so write code to close it
-//    // then remove the expanded class
-//    activeLink.removeClass(“.hide”);
-// } else {
-//     // link is not expanded so write code to open it
-//    // then add the expanded class
-//     activeLink.addClass(“.readmore”);
-// }
+// 	var activeLink = $(this);
+// 	if (activeLink.hasClass(".hide")) {
+// 		// link is already expanded so write code to close it
+// 		// then remove the expanded class
+// 		activeLink.removeClass(".hide");
+// 	} else {
+// 		// link is not expanded so write code to open it
+// 		// then add the expanded class
+// 		activeLink.addClass(".hide");
+// 	}
 
-// $(this).find(‘a.readmore').toggle();
-// }
+// 	$(this).find('a.readmore').toggle();
 
 // });
 
-
-$(document).ready(function() {
-    $("#show-this-on-click").accordion({collapsible: true, active: false});
+$("p.readmore a").on("click", function(e) {
+	e.preventDefault();
+	$("#show-this-on-click").slideToggle();
+	$(".readmore").addClass("hide");
+	$(".readless").removeClass("hide");
 });
+
+$("p.readless a").on("click", function(e) {
+	e.preventDefault();
+	$("#show-this-on-click").slideToggle();
+	$(".readless").addClass("hide");
+	$(".readmore").removeClass("hide");
+});
+
